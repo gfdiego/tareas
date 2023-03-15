875c166ae731cbf23e9755b233320c486485d2d2
gfdiego <126045984+gfdiego@users.noreply.github.com>
2023-03-14 21:12:12 -0600

    do dowhile

---
+++ do y do while.cpp
@@ -0,0 +1,54 @@
+# incluir  <iostream> _ _
+utilizando  el espacio de nombres  estándar ;
+int  main ( int argc, char  const *argv[])
+{
+    
+   /* int i=0;
+	
+	mientras(i<5){
+	
+		yo++;
+		cout<<i<<endl;
+	
+	}
+	int dec=10;
+	cout<<"_______________________"<<endl;
+	while(dic>0){
+		cout<<dec<<endl;
+	}
+    
+sistema("pausa"); */
+	
+
+/* int i=0;
+    mientras que (i>5)
+    {
+         cout<<"mientras"<<i<<endl;
+         yo++;
+    }
+    intii=0;
+    hacer
+    {
+        ii++;
+        cout<<"hacer mientras"<<ii<<endl;
+    } mientras (ii<11); */
+    
+    
+	
+/* char respuesta;
+hacer
+{
+    cout<<"desea ingresar otro valor(s/n)"<<endl;
+    cin>>respuesta;
+} while (respuesta=='s' || respuesta =='S'); */
+
+
+
+
+
+
+
+
+    devolver  0 ;
+}
+
---
+++ para.cpp
@@ -0,0 +1,89 @@
+# incluir  <iostream> _ _
+utilizando  el espacio de nombres  estándar ;
+int  main ( int argc, char  const *argv[])
+{
+    // código 1
+    /* int i=0;
+    para(i=5;i<8;i+=3){
+        cout<<i<<endl;
+    }
+    cout<<"i fuera"<<i<<endl;
+    sistema("pausa"); */
+    
+    // codigo 2
+    /* para(i=5;i<8;i+=3){
+        si (i==5)
+        {
+           romper;
+           cout<<"si"<<endl;
+        }
+        
+        cout<<i<<endl;
+    }
+    cout<<"fuera del for"<<i<<endl;
+    sistema("pausa"); */
+ 
+    // codigo 3
+    /* para(i=5;i<8;i+=3){
+        si (i==5)
+        {
+           cout<<"si"<<endl;
+           continuar;
+        }
+        
+        cout<<i<<endl;
+    }
+    cout<<"fuera del for"<<i<<endl;
+    sistema("pausa"); */
+
+     // codigo 4
+    /* para(i=5;i<8;i+=3){
+        si (i==5)
+        {
+           cout<<"si"<<endl;
+           continuar;
+        }
+        
+        cout<<i<<endl;
+    }
+    cout<<"fuera del for"<<i<<endl;
+    sistema("pausa"); */
+    // codigo 5
+    /* int datos[5]={10,40,100,250,3000};
+    para(int i=0;i<5;i++){
+            cout<<datos[i]<<endl;
+    }
+    sistema("pausa"); */
+     // codigo 6
+    /* int datos[]={8,12,35,200,1000}; // inicialización de matriz
+ 	//cout<<tamaño(datos)<<endl;
+ 	//cout<<tamaño(datos[0])<<endl;
+    int tama = (tamaño(datos)/tamaño(datos[0]));
+    
+    para (int i=0;i<tama;i++){
+    	cout<<datos[i]<<endl;
+	} */
+     // codigo 7
+    /* int inicio=0,fin=0,multiplicar=0;
+cout <<"Ingrese tabla inicio:";
+cin>>inicio;
+cout <<"Ingrese tabla fin:";
+cin>>aleta;
+for (int i=inicio; i<=fin;i++){
+	//cout<<i<<endl;
+	para (int ii=1; ii<=10;ii++){
+		//cout<<ii<<endl;
+	 multiplicar=i*ii;
+	 cout<<i<<" X "<<ii<<" = "<<multiplicar<<endl;
+	}
+	cout<<endl;
+}
+ 
+sistema("pausa"); */
+
+
+
+
+
+}
+
